7c6c55420c0c318060d4291a9e9f97f53d0ecd0a
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-04-22 12:43:19 +0100

    fix(desktop): adding proxy support to initial download
    Reading from environment variables and npm config.

---
+++ packages/desktop/proxy.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const npmConf = require("npm-conf");
+
+const conf = npmConf();
+
+module.exports = () => {
+    return process.env.HTTPS_PROXY ||
+        process.env.https_proxy ||
+        conf.get("https-proxy") ||
+        process.env.HTTP_PROXY ||
+        process.env.http_proxy ||
+        conf.get("proxy") ||
+        undefined;
+};
