e1e5eda29e41b15c2a11864a369aa522559dea5b
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-05-26 15:50:36 +0100

    chore(dev-deps): bump electron from 8.3.0 to 9.0.0
    Bump to latest.
    EnableWebSQL has disappeared so I've removed setting.

--- packages/desktop-core/src/main/windowing/browserWindowFactory.ts
+++ packages/desktop-core/src/main/windowing/browserWindowFactory.ts
@@ -16,7 +16,6 @@ export class BrowserWindowFactory implements IWindowFactory {
             allowRunningInsecureContent: false,
             contextIsolation: true,
             enableRemoteModule: false,
-            enableWebSQL: false,
             nodeIntegration: false,
             nodeIntegrationInSubFrames: false,
             nodeIntegrationInWorker: false,
@@ -30,46 +29,14 @@ export class BrowserWindowFactory implements IWindowFactory {
         };
 
         if (configuration !== undefined) {
+            const { affinity, ...rest } = configuration;
+
             options = {
-                acceptFirstMouse: configuration.acceptFirstMouse,
-                alwaysOnTop: configuration.alwaysOnTop,
-                autoHideMenuBar: configuration.autoHideMenuBar,
-                backgroundColor: configuration.backgroundColor,
-                center: configuration.centered,
-                closable: configuration.closeable,
-                darkTheme: configuration.darkTheme,
-                disableAutoHideCursor: configuration.disableAutoHideCursor,
-                focusable: configuration.focusable,
-                frame: configuration.frame,
-                fullscreen: configuration.fullscreen,
-                fullscreenable: configuration.fullscreenable,
-                hasShadow: configuration.hasShadow,
-                height: configuration.height,
-                icon: configuration.icon,
-                kiosk: configuration.kiosk,
-                maxHeight: configuration.maxHeight,
-                maxWidth: configuration.maxWidth,
-                maximizable: configuration.maximizable,
-                minHeight: configuration.minHeight,
-                minWidth: configuration.minWidth,
-                minimizable: configuration.minimizable,
-                movable: configuration.movable,
-                opacity: configuration.opacity,
-                resizable: configuration.resizeable,
-                show: configuration.show,
-                skipTaskbar: configuration.skipTaskbar,
-                tabbingIdentifier: configuration.tabbingIdentifier,
-                title: configuration.title,
-                titleBarStyle: configuration.titleBarStyle,
-                transparent: configuration.transparent,
+                ...rest,
                 webPreferences: {
                     ...webPreferences,
-                    affinity: configuration.affinity,
+                    affinity,
                 },
-                width: configuration.width,
-                x: configuration.x,
-                y: configuration.y,
-                zoomToPageWidth: configuration.zoomToPageWidth,
             };
         }
 
