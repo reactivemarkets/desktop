28dc29a6443cd60b72978a2c45dea99ab664ab9d
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-05-26 14:35:11 +0100

    chore: using cname for docs
    Updating to desktop.reactivemarkets.com

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    base: "/desktop/",
+    base: "/",
     title: "Desktop | Reactive Markets",
     description: "Desktop by Reactive Markets",
     head: [
         ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&display=swap" }],
         ["link", { rel: "icon", href: "/favicon.ico" }],
         ["link", { rel: "manifest", href: "/manifest.json" }],
-        ["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon.png" }],
-        ["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#4A90E2" }],
+        ["link", { rel: "apple-touch-icon", href: "/apple-touch-icon.png" }],
+        ["link", { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#4A90E2" }],
         ["meta", { name: "theme-color", content: "#4A90E2" }],
         ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
         ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }]
