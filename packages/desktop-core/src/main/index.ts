ceaef4eafb917cd312efa69c43cd90d4460c1952
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-04 13:32:40 +0100

    fix(core): handling squirrel events
    If squirrel is used the exe is called with args, we need to exit as
    soon as possible.

--- packages/desktop-core/src/main/index.ts
+++ packages/desktop-core/src/main/index.ts
@@ -1,22 +1,26 @@
 import { app } from "electron";
-
 import { cleanCommandLine, parseCommandLine } from "./configuration/commandLine";
 import { logger } from "./logging";
+import { checkForSquirrelEvents } from "./shortcuts";
 
-const appName = app.name;
-const isPackaged = app.isPackaged;
-const appPath = app.getAppPath();
-const version = app.getVersion();
-const locale = app.getLocale();
+if (checkForSquirrelEvents()) {
+    app.quit();
+} else {
+    const appName = app.name;
+    const isPackaged = app.isPackaged;
+    const appPath = app.getAppPath();
+    const version = app.getVersion();
+    const locale = app.getLocale();
 
-logger.verbose(`Application Name: ${appName}`);
-logger.verbose(`Application Path: ${appPath}`);
-logger.verbose(`Application Version: ${version}`);
-logger.verbose(`Application Locale: ${locale}`);
-logger.verbose(`Packaged: ${isPackaged}`);
+    logger.verbose(`Application Name: ${appName}`);
+    logger.verbose(`Application Path: ${appPath}`);
+    logger.verbose(`Application Version: ${version}`);
+    logger.verbose(`Application Locale: ${locale}`);
+    logger.verbose(`Packaged: ${isPackaged}`);
 
-const commandLine = cleanCommandLine(process.argv);
+    const commandLine = cleanCommandLine(process.argv);
 
-logger.verbose("Parsing command line: ", commandLine);
+    logger.verbose("Parsing command line: ", commandLine);
 
-parseCommandLine(commandLine, true);
+    parseCommandLine(commandLine, true);
+}
