9d4bc0499a771c5947d7cd43d077d70092ec9bc4
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-11 14:02:12 +0100

    docs(sdk): adding code docs to interfaces
    Adding usage descriptions.

--- packages/desktop-sdk/src/iLauncher.ts
+++ packages/desktop-sdk/src/iLauncher.ts
@@ -1,5 +1,9 @@
 import { IConfiguration } from "@reactivemarkets/desktop-types";
 
 export interface ILauncher {
+    /**
+     * Launch the given configuration.
+     * @param configuration The configuration
+     */
     launch(configuration: IConfiguration): Promise<IConfiguration>;
 }
--- packages/desktop-sdk/src/iRouter.ts
+++ packages/desktop-sdk/src/iRouter.ts
@@ -2,5 +2,4 @@ export interface IRouter {
     publish<T>(channel: string, payload: T): void;
     subscribe<T>(channel: string, listener: (payload: T) => void): void;
     unsubscribe<T>(channel: string, listener: (payload: T) => void): void;
-    unsubscribeAll(): void;
 }
