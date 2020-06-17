1cb4f8a1e7b6e945718fbfcbef9d3a3cde51ce93
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-17 17:14:29 +0100

    feat(core): launching applications that come in through open-url
    url is registered and launched.

--- packages/desktop-core/src/main/events/openUrlApplicationEventsHandler.ts
+++ packages/desktop-core/src/main/events/openUrlApplicationEventsHandler.ts
@@ -1,12 +1,31 @@
+import { ConfigurationKind } from "@reactivemarkets/desktop-types";
 import { App } from "electron";
-
-import { ReservedChannels } from "../../common";
-import { routerService } from "../router";
+import { uniqueNamesGenerator, adjectives, colors, animals } from "unique-names-generator";
+import { registryService } from "../registry";
+import { logger } from "../logging";
+import { configurationGenerator } from "../configuration";
+import { launcherService } from "../launcher";
 
 export const registerOpenUrlEventsHandler = (app: App) => {
-    app.on("open-url", (_, url) => {
-        routerService.send(ReservedChannels.application_openUrl, {
-            url,
-        });
+    app.on("open-url", async (event, url) => {
+        try {
+            event.preventDefault();
+
+            const configUrl = url.replace("desktop://", "https://");
+
+            const name = uniqueNamesGenerator({
+                dictionaries: [adjectives, colors, animals],
+            });
+
+            const configuration = await configurationGenerator.generate(ConfigurationKind.Application, name, configUrl);
+
+            await registryService.register(configuration);
+
+            const instance = await launcherService.launch(configuration);
+
+            logger.info(`Launched ${instance.kind}: ${instance.metadata.name}`);
+        } catch (error) {
+            logger.error(`Failed to launch from the protocol handler: ${error}`);
+        }
     });
 };
