5da91fd94fd8b37ff0a9c4a7424140cf3ba97494
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-18 21:59:06 +0100

    fix(core): using electron version instead of yargs
    The yargs version can sometimes be undefined.

--- packages/desktop-core/src/main/configuration/commandLine/yargsCommandLineParser.ts
+++ packages/desktop-core/src/main/configuration/commandLine/yargsCommandLineParser.ts
@@ -1,5 +1,5 @@
+import { app } from "electron";
 import * as yargs from "yargs";
-
 import {
     clearModule,
     describeModule,
@@ -22,6 +22,8 @@ const columnWidth = Math.min(yargs.terminalWidth(), 120);
  * @param exitProcess Exit if the command line is badly formed
  */
 export const parseCommandLine = (commandLine: string[], exitProcess = true) => {
+    const version = app.getVersion();
+
     return yargs
         .command(clearModule)
         .command(describeModule)
@@ -36,6 +38,7 @@ export const parseCommandLine = (commandLine: string[], exitProcess = true) => {
         .command(stopModule)
         .demandCommand(1, "Specify at least 1 command.")
         .recommendCommands()
+        .version(version)
         .exitProcess(exitProcess)
         .epilogue('Run "$0 COMMAND --help" for more information on a command.')
         .usage("Usage: $0 COMMAND [OPTIONS]\n\nA multi-window desktop runtime.")
