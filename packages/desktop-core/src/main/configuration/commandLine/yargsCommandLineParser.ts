cc11a218a8e16b33370ebd91e86723c86169900b
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-18 08:06:37 +0100

    fix(core): removing strict from command line
    Other options can be passed in on windows.

--- packages/desktop-core/src/main/configuration/commandLine/yargsCommandLineParser.ts
+++ packages/desktop-core/src/main/configuration/commandLine/yargsCommandLineParser.ts
@@ -36,7 +36,6 @@ export const parseCommandLine = (commandLine: string[], exitProcess = true) => {
         .command(stopModule)
         .demandCommand(1, "Specify at least 1 command.")
         .recommendCommands()
-        .strict()
         .exitProcess(exitProcess)
         .epilogue('Run "$0 COMMAND --help" for more information on a command.')
         .usage("Usage: $0 COMMAND [OPTIONS]\n\nA multi-window desktop runtime.")
