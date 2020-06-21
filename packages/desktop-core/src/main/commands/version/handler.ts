a7904c638e8ff62fe871308fcb8b43bf63991324
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-21 16:18:03 +0100

    feat(core): adding version command
    Prints out all process versions.

---
+++ packages/desktop-core/src/main/commands/version/handler.ts
@@ -0,0 +1,35 @@
+import { app } from "electron";
+import * as yaml from "js-yaml";
+import { logger } from "../../logging";
+import { IVersionOptions } from "./iVersionOptions";
+import { Output } from "../../configuration";
+
+export const handler = async ({ output }: IVersionOptions) => {
+    logger.verbose("Version command ran.");
+
+    try {
+        const { versions } = process;
+
+        switch (output) {
+            case Output.Json: {
+                const versionString = JSON.stringify(versions);
+
+                logger.info(versionString);
+                break;
+            }
+            case Output.Yaml:
+            default: {
+                const versionString = yaml.safeDump(versions, {
+                    indent: 2,
+                });
+
+                logger.info(versionString);
+            }
+        }
+
+        app.exit();
+    } catch (error) {
+        logger.error(`${error}`);
+        app.exit(1);
+    }
+};
---
+++ packages/desktop-core/src/main/commands/version/index.ts
@@ -0,0 +1,1 @@
+export * as versionModule from "./module";
