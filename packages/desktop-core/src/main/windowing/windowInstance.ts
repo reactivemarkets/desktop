b5a50bf3678dd4563344238ebf2ab453b0c4774f
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-06-28 09:17:25 +0100

    feat(core): adding window bounds to the application status
    Adding height, width, x, and y to status.

--- packages/desktop-core/src/main/windowing/windowInstance.ts
+++ packages/desktop-core/src/main/windowing/windowInstance.ts
@@ -1,5 +1,7 @@
 import { ApplicationState, IConfiguration } from "@reactivemarkets/desktop-types";
 import { BrowserWindow } from "electron";
+import { fromEventPattern, merge } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 import { TypedEmitter } from "tiny-typed-emitter";
 import { updateStatus } from "./configurationExtensions";
 
@@ -8,6 +10,7 @@ interface IWindowInstanceEvents {
 }
 
 export class WindowInstance extends TypedEmitter<IWindowInstanceEvents> {
+    #delay = 500;
     #configuration: IConfiguration;
     #instance: BrowserWindow;
 
@@ -29,11 +32,36 @@ export class WindowInstance extends TypedEmitter<IWindowInstanceEvents> {
     }
 
     private readonly attachEventHandlers = () => {
+        const move = fromEventPattern(
+            (h) => this.#instance.addListener("move", h),
+            (h) => this.#instance.removeListener("move", h),
+        );
+        const resize = fromEventPattern(
+            (h) => this.#instance.addListener("resize", h),
+            (h) => this.#instance.removeListener("resize", h),
+        );
+        const boundsChanged = merge(move, resize)
+            .pipe(debounceTime(this.#delay))
+            .subscribe({
+                next: this.onBoundsChange,
+            });
+
         this.#instance.once("closed", () => {
             this.#configuration = updateStatus(this.#configuration, {
                 state: ApplicationState.closed,
             });
             this.emit("status-update", this.#configuration);
+            boundsChanged.unsubscribe();
         });
+    };
+
+    private readonly onBoundsChange = () => {
+        const bounds = this.#instance.getNormalBounds();
+
+        this.#configuration = updateStatus(this.#configuration, {
+            ...bounds,
+        });
+
+        this.emit("status-update", this.#configuration);
     };
 }
