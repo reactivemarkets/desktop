b27872a77f56eb4eb09cac9796f2b7b944511715
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-07-01 17:55:36 +0100

    fix(desktop): correcting logging in notarize
    Prefixing the logs with notarize: to identify them.

--- packages/desktop/notarize.js
+++ packages/desktop/notarize.js
@@ -14,11 +14,11 @@ exports.default = function notarizing(context) {
     const appPath = `${appOutDir}/${context.packager.appInfo.productFilename}.app`;
 
     if (appleId === undefined || appleIdPassword === undefined) {
-        console.log("No AppleId or AppleIdPassword specified.");
+        console.log("notarize: no AppleId or AppleIdPassword specified.");
         return;
     }
 
-    console.log("Notarizing", appPath);
+    console.log(`notarize: ${appPath}`);
 
     return notarize({
         appBundleId,
