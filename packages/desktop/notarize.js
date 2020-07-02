1075a56a2ba020f0f313e7f0fbfeeb8cdc6ab9df
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-07-02 10:18:26 +0100

    chore(desktop): using appId from packager config
    Currently we're duplicating the appId, get it from the context instead.

--- packages/desktop/notarize.js
+++ packages/desktop/notarize.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 const { notarize } = require("electron-notarize");
 
 exports.default = function notarizing(context) {
-    const { electronPlatformName, appOutDir } = context;
+    const { electronPlatformName, appOutDir, packager } = context;
     if (electronPlatformName !== "darwin") {
         return;
     }
 
     const appleId = process.env.APPLE_ID;
     const appleIdPassword = process.env.APPLE_ID_PASSWORD;
-    const appBundleId = "com.reactivemarkets.desktop";
-    const appPath = `${appOutDir}/${context.packager.appInfo.productFilename}.app`;
+    const appBundleId = packager.config.appId;
+    const appPath = `${appOutDir}/${packager.appInfo.productFilename}.app`;
 
     if (appleId === undefined || appleIdPassword === undefined) {
         console.log("notarize: no AppleId or AppleIdPassword specified.");
