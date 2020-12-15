0a1d125dc81e93e03124656b82b0af5a51fd1d5b
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-12-15 23:01:02 +0000

    feat(core): adding security policies to launcher
    Checks configuration is allowed to launch.

--- packages/desktop-core/src/main/commands/start/iStartOptions.ts
+++ packages/desktop-core/src/main/commands/start/iStartOptions.ts
@@ -1,4 +1,4 @@
-interface IStartOptions {
+export interface IStartOptions {
     readonly context?: string;
     readonly file?: string[];
     readonly url?: string[];
--- packages/desktop-core/src/main/launcher/index.ts
+++ packages/desktop-core/src/main/launcher/index.ts
@@ -6,35 +6,61 @@ import { ApplicationLauncherService } from "./applicationLauncherService";
 import { CompositeLauncherService } from "./compositeLauncherService";
 import { ExternalLauncherService } from "./externalLauncherService";
 import { ILauncherService } from "./iLauncherService";
-import { NodeServiceLauncherService } from "./nodeServiceLauncherService";
+import { ServiceLauncherService } from "./serviceLauncherService";
+import { registryService } from "../registry";
 import { SessionLauncherService } from "./sessionLauncherService";
 import { TrayLauncherService } from "./trayLauncherService";
-import { trayService } from "../tray";
 import { StorageLauncherService } from "./storageLauncherService";
 import { storageService } from "../storage";
+import { trayService } from "../tray";
 import { UpdatePolicyLauncherService } from "./updatePolicyLauncherService";
 import { updateService } from "../updates";
+import { ApplicationSecurityPolicyLauncherService } from "./applicationSecurityPolicyLauncherService";
+import { ExternalSecurityPolicyLauncherService } from "./externalSecurityPolicyLauncherService";
+import { ServiceSecurityPolicyLauncherService } from "./serviceSecurityPolicyLauncherService";
+import { SecurityPolicyLauncherService } from "./securityPolicyLauncherService";
 
 const applicationLauncherService = new ApplicationLauncherService(logger, windowService);
 
+const applicationPolicyLauncherService = new ApplicationSecurityPolicyLauncherService(
+    logger,
+    applicationLauncherService,
+    registryService,
+);
+
 const externalLauncherService = new ExternalLauncherService(logger, processExec);
 
-const serviceLauncherService = new NodeServiceLauncherService(logger, processFork);
+const externalPolicyLauncherService = new ExternalSecurityPolicyLauncherService(
+    logger,
+    externalLauncherService,
+    registryService,
+);
+
+const serviceLauncherService = new ServiceLauncherService(logger, processFork);
+
+const servicePolicyLauncherService = new ServiceSecurityPolicyLauncherService(
+    logger,
+    serviceLauncherService,
+    registryService,
+);
 
 const sessionLauncherService = new SessionLauncherService(logger, sessionService);
 
-const trayLauncherService = new TrayLauncherService(logger, trayService);
-
 const storageLauncherService = new StorageLauncherService(logger, storageService);
+
+const trayLauncherService = new TrayLauncherService(logger, trayService);
 
 const updateLauncherService = new UpdatePolicyLauncherService(logger, updateService);
 
+const securityPolicyLauncherService = new SecurityPolicyLauncherService();
+
 export const launcherService: ILauncherService = new CompositeLauncherService(
-    applicationLauncherService,
-    serviceLauncherService,
+    applicationPolicyLauncherService,
+    servicePolicyLauncherService,
     trayLauncherService,
     storageLauncherService,
     sessionLauncherService,
-    externalLauncherService,
     updateLauncherService,
+    securityPolicyLauncherService,
+    externalPolicyLauncherService,
 );
